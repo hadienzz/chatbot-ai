5274b331b364af20ae786b1b6ee8ca37a5c98ff8
hadienzz <hadinpramiadi@gmail.com>
2025-05-21 18:06:40 +0700

    save local change before pull

--- src/components/MessageSection.jsx
+++ src/components/MessageSection.jsx
@@ -10,7 +10,7 @@ const MessageSection = () => {
             {!resultIsShowing && loading ? <p>Loading</p> :
                 chatHistory.messages.map((item, idx) => (
                     <div className={`flex items-center ${item.role === 'user' ? 'justify-end' : 'justify-start'}`} key={idx}>
-                        <div className={`${item.role === 'user' ? " bg-[#f0f4f9] inline-flex text-right  py-2 px-6 rounded-md my-4" : ''}`}>
+                        <div className={`${item.role === 'user' ? " bg-[#f0f4f9] inline-flex text-right py-2 px-6 rounded-md my-4" : ''}`}>
                             <ReactMarkdown>
                                 {item.parts[0].text}
                             </ReactMarkdown>
